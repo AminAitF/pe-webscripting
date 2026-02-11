da1cace3f80864729084946ccc91b094b670a1bb
AminAitF <12202700@student.pxl.be>
2026-02-11 11:35:00 +0100

    Add files via upload

---
+++ JavaScript/les1/oef1.js
@@ -0,0 +1,16 @@
+const prompt = require('prompt-sync')();
+
+function isGetal(aantalLijnen) {
+    return !isNaN(parseFloat(aantalLijnen));
+}
+
+let aantallijnen = prompt('Geef het aantal sterren (0-9)');
+while (!isGetal(aantallijnen) || aantallijnen < 0 || aantallijnen > 9) {
+    console.log("Foutieve nummer")
+    aantalLijnen = prompt('Geef het aantal sterren (0-9)');
+}
+
+for (let i = 1; i <= aantallijnen; i++) {
+    console.log("#".repeat(i));
+}
+
